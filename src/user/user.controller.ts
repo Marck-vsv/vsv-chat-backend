fc5c5c3f3202d17295b14c420231f59c92a5df5b
Marck-vsv <marck.vinisv@outlook.com>
2024-07-11 09:53:26 -0300

    Added get user by username method/endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { UserService } from './user.service';
 import { Prisma } from '@prisma/client';
 
@@ -6,6 +6,11 @@ import { Prisma } from '@prisma/client';
 export class UserController {
   private readonly userService = new UserService();
 
+  @Get(':username')
+  async getUser(@Param('username') username: string) {
+    return await this.userService.getByUserName(username)
+  }
+  
   @Post('create')
   async createOne(@Body() data: Prisma.UserCreateInput): Promise<any> {
     return await this.userService.createUser(data);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient, Prisma } from '@prisma/client';
+import { Injectable, NotFoundException, ServiceUnavailableException } from "@nestjs/common";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -8,8 +8,41 @@ export class UserService {
   private queryIn = prisma.user;
 
   async createUser(data: Prisma.UserCreateInput) {
-    return this.queryIn.create({
-      data,
-    });
+    try {
+      const create_user = await this.queryIn.create({
+        data
+      });
+      
+      if (!create_user) {
+        throw new ServiceUnavailableException('Could not create user');
+      }
+      
+      return create_user;
+    } catch (error) {
+      console.error('Could not create user: ', error);
+      throw error
+    }
+  }
+  
+  async getByUserName(username: string) {
+    try {
+      const user = await this.queryIn.findFirst({
+        where: {
+          user_name: username
+        },
+        select: {
+          user_name: true
+        }
+      })
+
+      if (!user) {
+        throw new NotFoundException('User not found')
+      }
+
+      return user.user_name;
+    } catch (error) {
+      console.error('Error fetching user:', error)
+      throw error
+    }
   }
 }
