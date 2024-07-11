d2c5b16b53b4806fdd767fd4ef8af6d8534c62ef
Marck-vsv <marck.vinisv@outlook.com>
2024-07-11 11:00:23 -0300

    Added message and chat resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ChatModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
